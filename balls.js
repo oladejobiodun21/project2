ea9aa02aadbdd67ac0d158cc67432904a3543550
Oladejo Khaleed <oladejokhaleed2@gmail.com>
2024-11-04 02:35:54 -0800

    new changes

--- balls.js
+++ balls.js
@@ -1,10 +1,11 @@
-const ball = document.querySelector(".toggle-balls");
+const balls = document.querySelector(".toggle-balls");
 const items = document.querySelectorAll(
-  ".container,.movie-list-title,.nav-menu,.sidebar,.left-menu-icon,.toggles, .nav-menu li,.po,.movie-item-content, .ed,.body, :root, .search .fa-search, .search input::placeholder,.search, po p, .sqi2 h1, .sqi2 h1:hover,.sqi3 h1, .sqi3 h1:hover,#anny"
+  ".container,.movie-list-title,.nav-menu,.sidebar,.left-menu-icon,.toggle, .nav-menu li,.po,.movie-item-content, .ed,.body, :root, .search .fa-search, .search input::placeholder,.search, po p, .sqi2 h1, .sqi2 h1:hover,.sqi3 h1, .sqi3 h1:hover,#anny"
 );
-ball.addEventListener("click", () => {
+
+balls.addEventListener("click", () => {
   items.forEach((item) => {
-    item.classList.toggles("active");
+    item.classList.toggle("active");
   });
-  ball.classList.toggles("active");
+  balls.classList.toggle("active");
 });
